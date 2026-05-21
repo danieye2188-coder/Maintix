db47596cfa9da7712caba16aed80b59b1e77f3b7
DaRaLe <danieye2188@gmail.com>
2026-05-21 07:11:48 +0200

    Create firebase.js

---
+++ firebase.js
@@ -0,0 +1,16 @@
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-app.js";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyANLs4XPMJb3tZ2TejuWg_kTCGK9zL_LtM",
+  authDomain: "game-2168b.firebaseapp.com",
+  projectId: "game-2168b",
+  storageBucket: "game-2168b.firebasestorage.app",
+  messagingSenderId: "871647963096",
+  appId: "1:871647963096:web:9279fb718663e8e63834ad"
+};
+
+const app = initializeApp(firebaseConfig);
+
+console.log("Firebase verbunden");
+
+export { app };
